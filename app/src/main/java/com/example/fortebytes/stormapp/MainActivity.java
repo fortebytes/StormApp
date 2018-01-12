0e7096bda06902cd45d8def1c47749cae76a4e2e
Sierra Esteban <SierraEsteban@JohnDeere.com>
2018-01-12 14:24:01 -0600

    List view for main activity

--- app/src/main/java/com/example/fortebytes/stormapp/MainActivity.java
+++ app/src/main/java/com/example/fortebytes/stormapp/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.fortebytes.stormapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ListView lv_news_feed;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv_news_feed = (ListView) findViewById(R.id.lv_homepage);
+
+        // Instanciating an array list (you don't need to do this,
+        // you already have yours).
+        List<String> your_array_list = new ArrayList<String>();
+        your_array_list.add("foo");
+        your_array_list.add("bar");
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        lv_news_feed.setAdapter(arrayAdapter);
+
     }
+
+    public class Item {
+
+        private String mImageUrl;
+        private String mTitle;
+        private String mDescription;
+
+        // constructor, getters and setters elided
+
+
+    }
+
 }
